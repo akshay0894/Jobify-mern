a5dea3b406eeb78d6aafc80494f5a7da63b09e4b
Akshay Mittal <akshay.mittal0894@gmail.com>
2025-03-23 00:14:54 +0530

    env template

---
+++ envTemplate.js
@@ -0,0 +1,8 @@
+// PORT=5100
+// NODE_ENV=development
+// MONGO_URL= mongodb+srv://db_user:WhH4n1c1P0oXnMPV@cluster0.020wd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+// JWT_SECRET=secret
+// JWT_EXPIRES_IN=1d
+// CLOUD_NAME=''
+// CLOUD_API_KEY=''
+// CLOUD_API_SECRET=''
